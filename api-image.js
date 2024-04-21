a45e197ca7402eb175e269269c95911354f24ed4
masterdpro <82127205+masterdpro@users.noreply.github.com>
2024-04-21 17:34:07 -0400

    Update api-image.js

--- api-image.js
+++ api-image.js
@@ -1,19 +1,11 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const path = require("path");
 const fs = require("fs");
-const sharp = require("sharp");
-const axios = require("axios");
-const { promisify } = require("util");
-const readdirAsync = promisify(fs.readdir);
-const { getInstaData } = require("./main");
-const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 
-const secret_key = "1234"; // The beautiful private key
+const secret_key = "YourSecretKey"; // The beautiful private key
 const sharexdir = "image/"; // Your folder
-const domain_url = "https://cdn.discords.ca/"; // Your domain name
+const domain_url = "https://your.domain.name/"; // Your domain name
 const lengthofstring = 7;
 
 function randomString(length) {
@@ -32,140 +24,40 @@ app.use(fileUpload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-async function getImageTypeFromBuffer(url) {
-  try {
-    const response = await axios.get(url);
-    const coneType = response.headers["content-type"];
-    console.log(coneType);
-    if (coneType.includes("image")) {
-      return coneType.split("/")[1];
-    } else {
-      return null;
-    }
-  } catch (error) {
-    return console.error("Error downloading image:", error.message);
-  }
-}
+// Route to handle GET request for downloading an image
 
-app.get("/image", async (req, res) => {
-  const name = req.query.name;
-  const urlString = req._parsedUrl.search;
-  console.log(urlString);
-
-  const httpIndex = urlString.indexOf("http");
-  let url;
-
-  // If 'http' is found, extract everything after it
-  if (httpIndex !== -1) {
-    url = urlString.substring(httpIndex);
-  } else {
-    console.log("No 'http' found in the URL string.");
-    return res.status(400).send("Invalid URL");
-  }
-
-  try {
-    // Transform the URL to a base64 string
-    const response = await axios.get(url, { responseType: "arraybuffer" });
-    const urlBuffer = Buffer.from(response.data, "binary").toString("base64");
-
-    // Check if the image is a GIF
-    const imageType = await getImageTypeFromBuffer(url);
-    if (!imageType) {
-      return res.status(400).send("Unknown image type");
-    }
-
-    if (imageType !== "gif") {
-      // If it's not a GIF, process it as a static image
-      const buffer = Buffer.from(urlBuffer, "base64");
-      const image = sharp(buffer);
-
-      const allImagesFiles = await readdirAsync("./image");
-      const allImages = allImagesFiles.map((file) => file.split(".")[0]);
-
-      if (allImages.includes(`${name}`)) {
-        return res.status(409).send("Image name already used");
-      }
-
-      await image.toFile(`./image/${name}.${imageType}`, (err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error saving the image");
-        } else {
-          res.send(`Image ${name} uploaded successfully`);
-        }
-      });
-    } else {
-      // If it's a GIF, save it directly
-      const gifBuffer = Buffer.from(urlBuffer, "base64");
-      fs.writeFile(`./image/${name}.gif`, gifBuffer, (err) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send("Error saving the GIF");
-        } else {
-          res.send(`GIF ${name} uploaded successfully`);
-        }
-      });
-    }
-  } catch (error) {
-    console.error("Error downloading image:", error.message);
-    res.status(500).send("Error downloading the image");
-  }
-});
-
+//Route to handle ShareX uploads
 app.post("/upload", (req, res) => {
   if (req.body.secret) {
     if (req.body.secret === secret_key) {
-      const filename = randomString(lengthofstring);
-      const file = req.files.sharex;
-      const fileType = file.name.split(".").pop();
+      const filename = randomString(lengthofstring); // Generate random string for the file name
+      const file = req.files.sharex; // Get the file from the request
+      const fileType = file.name.split(".").pop(); // Get the file extension
 
       file.mv(`${sharexdir}${filename}.${fileType}`, (err) => {
         if (err) {
           return res
             .status(500)
-            .send("File upload failed - CHMOD/Folder doesn't exist?");
+            .send("File upload failed - CHMOD/Folder doesn't exist?"); // Not enought permission or the folder doesn't exist
         }
-        res.send(`${domain_url}${filename}.${fileType}`);
+        res.send(`${domain_url}${sharexdir}${filename}.${fileType}`);
       });
     } else {
-      res.send("Invalid Secret Key");
+      res.send("Invalid Secret Key"); //You putted the wrong key in shareX config
     }
   } else {
-    res.send("No post data received");
+    res.send("No post data received"); //You didn't put any key in shareX config
   }
 });
-app.get("/image/:name/:url", (req, res) => {
-  const name = req.params.name;
-  let imageBuffer;
-  try {
-    imageBuffer = Buffer.from(url, "base64");
-  } catch (error) {
-    console.error("Error decoding base64 string:", error);
-    return res.status(400).send("Invalid base64 string");
-  }
-  const allImage = fs.readdirSync("./image");
-  // Check if the image file already exists
-  if (allImage.includes(`${name}.jpg`)) {
-    return res.status(409).send("Image name already used");
-  }
 
-  fs.writeFile(`./image/${name}.jpg`, imageBuffer, (err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("Error saving the image");
-    } else {
-      res.send(`Image ${name} uploaded successfully`);
-    }
-  });
-});
 
-// Route to handle GET request for downloading an image
+//Route to handle GET request for downloading an image
 app.get("/image/:name", (req, res) => {
   const name = req.params.name;
-
+//Get all the files in the image folder
   const allImagesFiles = fs.readdirSync("./image");
 
-  // check if the name already contain the extension
+
   if (name.includes(".")) {
     if (allImagesFiles.includes(`${name}`)) {
       return res.sendFile(`./image/${name}`, {
@@ -178,7 +70,7 @@ app.get("/image/:name", (req, res) => {
         },
       });
     } else {
-      return res.status(404).send("Image not found");
+      return res.status(404).send("Image not found"); //The image doesn't exist
     }
   }
 
